da678db0114563200306922050ba18874217896c
sAchinStar <sachin680125@gmail.com>
2024-12-18 10:19:37 +0530

    feat: add reusable Modal component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@
 
 import Navbar from "./Components/Navbar";
 import Card from './Components/Card'
+import Modal from "./Components/Modal";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="container text-center bg-info-subtle">
+        <h1 className="my-5">AI-Powered Journal with Mood Analysis</h1>
+        <h2 className="my-5">How's your day</h2>
+        {/* <!-- Button trigger modal --> */}
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          Write
+        </button>
+        <Modal/>
+      </div>
       <div className="container">
         <Card />
       </div>
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,27 @@
+function Modal() {
+    return (
+        <div className="container text-center bg-info-subtle">
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Write about your day/thoughts/feelings</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <textarea className="form control" cols={101} rows={20} placeholder="Write here...">
+                            </textarea>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
